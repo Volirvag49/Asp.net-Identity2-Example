e91c3f3b37e11467b96321472a3868492447abec
Volirvag49 <lirikvolirvag@gmail.com>
2017-10-17 15:10:28 +1100

    Добавьте файлы проекта.

---
+++ Identity2Example/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Identity2Example.Startup))]
+namespace Identity2Example
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
